780b1a209ceabf7dd1f7c77f62f884b7a4d8c06a
yahavfuchs <yahavf6@gmail.com>
2018-11-03 20:35:08 +0200

    add google login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 import {LoginPage} from "../pages/login/login";
+
+import {SocialLoginModule, AuthServiceConfig, GoogleLoginProvider} from "angular5-social-login";
+
+// Configs
+export function getAuthServiceConfigs() {
+  let config = new AuthServiceConfig(
+    [
+      {
+        id: GoogleLoginProvider.PROVIDER_ID,
+        provider: new GoogleLoginProvider("1063446402194-rnlkor9b1b9o4v7l602t9uq018ib0vko.apps.googleusercontent.com")
+      }
+    ]);
+  return config;
+}
 
 @NgModule({
   declarations: [
@@ -18,6 +32,7 @@ import {LoginPage} from "../pages/login/login";
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    SocialLoginModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -28,7 +43,9 @@ import {LoginPage} from "../pages/login/login";
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    {provide: AuthServiceConfig, useFactory: getAuthServiceConfigs}
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -8,6 +8,12 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+import {
+  AuthService,
+  GoogleLoginProvider
+} from 'angular5-social-login';
+import {HomePage} from "../home/home";
+
 @IonicPage()
 @Component({
   selector: 'page-login',
@@ -15,11 +21,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  user;
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private socialAuthService: AuthService ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
 
+  public socialSignIn(socialPlatform : string) {
+    let socialPlatformProvider;
+    if(socialPlatform == "facebook"){
+      // socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
+    }else if(socialPlatform == "google"){
+      socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+    }
+
+    this.socialAuthService.signIn(socialPlatformProvider).then(
+      (userData) => {
+        this.user = userData;
+        console.log(socialPlatform+" sign in data : " , this.user);
+        // Now sign-in with userData
+        if (this.user) {
+          this.navCtrl.push(HomePage, {user: this.user});
+          return;
+        }
+      }
+    );
+  }
+
 }
+
+
